be1bbbe36c3acd51527b56f80695d73be452c405
NataliiaKorotenko <119405955+NataliiaKorotenko@users.noreply.github.com>
2023-05-12 14:20:43 +0200

    import

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
